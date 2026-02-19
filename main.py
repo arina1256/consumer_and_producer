2c61beb8072aa8ef2a85c666b436df1ededa2373
arina1256 <126387228+arina1256@users.noreply.github.com>
2026-02-19 18:28:48 +0300

    Update main.py
    добавление многопоточности

--- main.py
+++ main.py
@@ -2,126 +2,134 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import glob
 import os
-import threading
+import multiprocessing
 import queue
-import time
+
+TASK_QUEUE_SIZE = 20
+THUMBNAIL_SIZE = (640, 480)
+LOADING_DELAY = 200
+NUMBER_OF_CONSUMERS = os.cpu_count()-1
+RESULTS_CHECK_DELAY = 10
+
+def consumer_task(task_queue, result_queue, stop_event):
+    while not stop_event.is_set():
+        try:
+            task = task_queue.get(timeout=1)
+            if task is None:
+                break
+            
+            i, path = task
+            
+            img = Image.open(path)
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            img.thumbnail(THUMBNAIL_SIZE)
+            
+            result_queue.put((i, img.tobytes(), img.size))
+
+        except queue.Empty:
+            continue
+        except Exception as err:
+            print(f"Ошибка: {err}")
+
 
 class JP2Viewer:
-    def __init__(self, directory_path, fps=30):
+    def __init__(self, directory_path, fps):
         self.files = glob.glob(os.path.join(directory_path, "*.jp2"))
         self.files.sort()
         
         self.number_of_frames = len(self.files)
+        if self.number_of_frames==0:
+            print("Пустая папка")
+            return
         
         self.fps = fps
-        self.delay = int(1000 / fps)
-        self.running = True
+        self.delay = 1000//fps if fps>0 else 66
+        self.stop_event = multiprocessing.Event()
         
-        self.num_consumers = 2
+        self.num_consumers = NUMBER_OF_CONSUMERS
         
-        self.task_queue = queue.Queue(maxsize=20)
-        self.result_queue = queue.Queue(maxsize=10)
+        self.task_queue = multiprocessing.Queue(maxsize=TASK_QUEUE_SIZE)
+        self.result_queue = multiprocessing.Queue()
         
         self.frames = [None]*self.number_of_frames
-        self.frames_lock = threading.Lock()
         self.loaded_number = 0
-        
         self.current_index = 0
         
         self.window = tk.Tk()
         self.window.title("JP2 Viewer")
         self.window.protocol("WM_DELETE_WINDOW", self.exit)
-        
         self.label = tk.Label(self.window, bg='pink')
         self.label.pack()
-        
         self.info = tk.Label(self.window, text="Инициализация")
         self.info.pack()
         tk.Button(self.window, text="Выход", command=self.exit).pack()
         
-        self.start_threads()
+        self.start_processings()
         self.check_loading()
         self.window.mainloop()
     
-    def start_threads(self):
-        self.producer = threading.Thread(target=self.producer_task)
-        self.producer.daemon = True
+    @staticmethod
+    def producer_task_static(file_list, task_queue, num_consumers, stop_event):
+        for i, path in enumerate(file_list):
+            if stop_event.is_set():
+                break
+            try:
+                task_queue.put((i, path), timeout=1)
+            except Exception as err:
+                print(f"Ошибка: {err}")
+        for _ in range(num_consumers):
+            try:
+                task_queue.put(None, timeout=1)
+            except Exception as err:
+                print(f"Ошибка: {err}")
+    
+    def start_processings(self):
+        self.producer = multiprocessing.Process(
+            target=JP2Viewer.producer_task_static,
+            args=(self.files, self.task_queue, self.num_consumers, self.stop_event)
+        )
         self.producer.start()
         
         self.consumers = []
         for i in range(self.num_consumers):
-            consumer = threading.Thread(target=self.consumer_task, args=(i,))
-            consumer.daemon = True
+            consumer = multiprocessing.Process(target=consumer_task, args=(self.task_queue, self.result_queue, self.stop_event))
             consumer.start()
             self.consumers.append(consumer)
+
+        self.window.after(0, self.collect_results)
+    
+    def collect_results(self):
+        try:
+            while True:
+                i, data, size = self.result_queue.get_nowait()
+                img = Image.frombytes('RGB', size, data)
+                
+                self.frames[i] = img
+                self.loaded_number += 1
+                
+                if i == 0:
+                    self.show_frame(i)
+        except queue.Empty:
+            pass
         
-        self.collector = threading.Thread(target=self.collector_task)
-        self.collector.daemon = True
-        self.collector.start()
-    
-    def producer_task(self):
-        for i, path in enumerate(self.files):
-            if not self.running:
-                break
-            try:
-                self.task_queue.put((i, path), timeout=1)
-            except:
-                pass
-        
-        for _ in range(self.num_consumers):
-            try:
-                self.task_queue.put(None, timeout=1)
-            except:
-                pass
-    
-    def consumer_task(self, worker_id):
-        while self.running:
-            try:
-                task = self.task_queue.get(timeout=1)
-                if task is None:
-                    break
-                
-                i, path = task
-                
-                img = Image.open(path)
-                if img.mode != 'RGB':
-                    img = img.convert('RGB')
-                img.thumbnail((640, 480))
-                
-                self.result_queue.put((i, img))
-                
-            except queue.Empty:
-                continue
-            except Exception as e:
-                print(f"Ошибка: {e}")
-    
-    def collector_task(self):
-        while self.running:
-            try:
-                i, img = self.result_queue.get(timeout=1)
-                
-                with self.frames_lock:
-                    self.frames[i] = img
-                    self.loaded_number += 1
-                    
-                    if i == 0:
-                        self.window.after(0, self.show_frame, i)
-                
-            except queue.Empty:
-                continue
+        if not self.stop_event.is_set():
+            self.window.after(RESULTS_CHECK_DELAY, self.collect_results)
     
     def check_loading(self):
-        if self.frames[0] is not None:
+        first_frame = self.frames[0]
+        loaded = self.loaded_number
+
+        if first_frame is not None:
             self.show_frame(0)
             self.play()
         else:
-            self.info.config(text=f"Загрузка первого кадра({self.loaded_number}/{self.number_of_frames})")
-            self.window.after(200, self.check_loading)
+            self.info.config(text=f"Загрузка первого кадра({loaded}/{self.number_of_frames})")
+            self.window.after(LOADING_DELAY, self.check_loading)
     
     def show_frame(self, index):
         try:
-            with self.frames_lock:
-                img = self.frames[index]
+            img = self.frames[index]
             
             if img:
                 photo = ImageTk.PhotoImage(img)
@@ -129,21 +137,22 @@ class JP2Viewer:
                 self.label.image = photo
                 self.current_index = index
                 
-        except Exception as e:
-            print(f"Ошибка: {e}")
+        except Exception as err:
+            print(f"Ошибка: {err}")
     
     def play(self):
-        if not self.running:
+        if self.stop_event.is_set():
             return
         
         next_index = (self.current_index+1)%self.number_of_frames
         
-        with self.frames_lock:
-            next_img = self.frames[next_index]
-            loaded = self.loaded_number
+        next_img = self.frames[next_index]
+        loaded = self.loaded_number
         
         if next_img:
             self.show_frame(next_index)
+        else:
+            next_index = self.current_index
         
         self.info.config(text=f"Кадр {next_index + 1}/{self.number_of_frames}, Загружено: {loaded}")
         
@@ -151,15 +160,23 @@ class JP2Viewer:
     
     def exit(self):
         print("Завершение")
-        self.running = False
+
+        self.stop_event.set()
+        for _ in range(self.num_consumers):
+            self.task_queue.put(None)
+
+        self.producer.join()
+        for i in self.consumers:
+            i.join()
+
         self.window.quit()
         self.window.destroy()
 
+
 if __name__ == "__main__":
-    folder = "converted"
-    
-    if os.path.exists(folder):
+    directory = "converted"
+    if os.path.exists(directory):
         print("Запуск")
-        viewer = JP2Viewer(folder, fps=15)
+        viewer = JP2Viewer(directory, fps=15)
     else:
-        print(f"Папка {folder} не найдена")
+        print(f"Папка {directory} не найдена")
